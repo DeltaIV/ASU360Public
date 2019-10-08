803278111597e2785dc485eb6f542fbb5810e9b6
D Mac <dcmoller@asu.edu>
2019-10-08 12:28:29 -0700

    Assignment finished

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -5,6 +5,7 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
@@ -16,14 +17,16 @@ public class AddingMachine {
 	
 	public void add (int value) {
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
 		total = total - value;
+		history = history + " - " + value;
 	}
 		
 	public String toString () {
-		return "0 + " + "";
+		return history;
 	}
 
 	public void clear() {
--- src/cse360assign2/AddingMachineTest.java
+++ src/cse360assign2/AddingMachineTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class AddingMachineTest {
 
 	AddingMachine test = new AddingMachine();
+	AddingMachine test2 = new AddingMachine();
 	
 	@Test
 	void testAddingMachine1() {
@@ -14,6 +15,15 @@ class AddingMachineTest {
 		test.subtract(3);
 		test.add(3);
 		assertTrue(test.getTotal() == 5);
+	}
+	
+	@Test
+	void testAddingMachine2() {
+		test.add(6);
+		test.subtract(2);
+		test.add(9);
+		test.subtract(4);
+		System.out.println(test.toString());
 	}
 
 }
