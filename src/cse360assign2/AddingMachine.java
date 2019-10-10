4ba93ea76d65d5dd758f3264a56e949c1b495c0c
dmoller <dmollerjr@outlook.com>
2019-10-09 23:47:10 -0700

    Added what I hope is right so i dont get assaulted in my grade again

--- src/cse360assign2/AddingMachine.java
+++ src/cse360assign2/AddingMachine.java
@@ -1,43 +1,68 @@
-/**  
-* AddingMachine.java - Add and subtract a value
-* <p>
-* 
-* !Modified by!
-* @author  David Moller 
-* 10/5/2019
-* Class: CSE360, Friday, 2019
-* @version %I%, %G%
-*/ 
+/**
+ * AddingMachine.java - Add and subtract a value
+
+ * @author  David Moller 
+ * 10/5/2019
+ * Class: 85141
+ * @version %I%, %G%
+ */ 
 
 package cse360assign2;
 
+/**
+ * This method performs math operations on the total based on numbers passed
+ * in to the methods 
+ * @param  int value
+ * @return string of the operations performed
+ */
 public class AddingMachine {
 
+	/**
+	 * This is the total
+	 */
 	private int total;
+	/**
+	 * This is the string to return
+	 */
 	private String history = "0";
-	
+	/**
+	   * creates a new Adding Machine.
+	   */
 	public AddingMachine () {
 		total = 0;
 	}
-	
+	/**
+	  * This returns the total
+	  */
 	public int getTotal () {
 		return total;
 	}
-	
+	/**
+	  * This adds to the total
+	  * @param int set by user
+	  */
 	public void add (int value) {
 		total = total + value;
 		history = history + " + " + value;
 	}
-	
+	/**
+	  * This subtracts from the total
+	  * @param int set by user
+	  */
 	public void subtract (int value) {
 		total = total - value;
 		history = history + " - " + value;
 	}
-		
+	/**
+	  * This returns the ooerations to the total
+	  */
 	public String toString () {
 		return history;
 	}
 
+	/**
+	  * This clears
+	  */
 	public void clear() {
 	
 	}
